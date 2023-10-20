0d367e9a76a1ddb25693531d682708d36101510d
Mihir Chitre <chitre.m@northeastern.edu>
2023-10-19 20:26:47 -0400

    Fixed error in lab code

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -4,11 +4,13 @@ import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todo/TodoList";
+import TodoItem from "./todo/TodoItem";
 
 function Assignment3() {
  return (
    <div>
      <h1>Assignment 3</h1>
+     <TodoItem/>
      <TodoList/>
      <ConditionalOutput/>
      <Styles/>
